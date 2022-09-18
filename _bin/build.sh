fc025d36ef2e7dcc132d2ce33d67ef7bba5d9672
Anna Wilson <git@annawilson.eu>
2022-09-18 15:08:50 +0100

    group officer details

--- _bin/build.sh
+++ _bin/build.sh
@@ -1,4 +1,4 @@
 curl -o _data/remote/regnum.json https://scripts.drachenwald.sca.org/json/regnum-officers-box.json
-curl -o _data/remote/calendar.json https://scripts.drachenwald.sca.org/json/calendar.json
+curl https://scripts.drachenwald.sca.org/json/calendar.json |  sed -e ':a' -e ' s/\\n\\n\\n//g; ta' | sed -e ':a' -e ' s/\\n/\<br \/\>/g; ta' > _data/remote/calendar.json 
 curl -o _data/remote/groups.json https://scripts.drachenwald.sca.org/json/regnum-groups.json
 JEKYLL_ENV=production bundle exec jekyll build
--- _bin/entrypoint.sh
+++ _bin/entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /build
 curl -o _data/remote/regnum.json https://scripts.drachenwald.sca.org/json/regnum-officers-box.json
-curl -o _data/remote/calendar.json https://scripts.drachenwald.sca.org/json/calendar.json
+curl https://scripts.drachenwald.sca.org/json/calendar.json |  sed -e ':a' -e ' s/\\n\\n\\n//g; ta' | sed -e ':a' -e ' s/\\n/\<br \/\>/g; ta' > _data/remote/calendar.json 
 curl -o _data/remote/groups.json https://scripts.drachenwald.sca.org/json/regnum-groups.json
 bundle install
 bundle exec jekyll serve --watch --livereload --host 0.0.0.0
