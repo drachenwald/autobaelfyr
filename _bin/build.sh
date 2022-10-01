c4cd1844c5dfa33bf640721d75d5f3e2d4bdb731
Anna Wilson <git@annawilson.eu>
2022-10-01 17:46:20 +0200

    was using old data

--- _bin/build.sh
+++ _bin/build.sh
@@ -1,5 +1,5 @@
-curl -o _data/remote/regnum.json https://scripts.drachenwald.sca.org/json/regnum-officers-box.json
-curl https://scripts.drachenwald.sca.org/json/calendar.json |  sed -e ':a' -e ' s/\\n\\n\\n//g; ta' | sed -e ':a' -e ' s/\\n/\<br \/\>/g; ta' > _data/remote/calendar.json 
-curl -o _data/remote/groups.json https://scripts.drachenwald.sca.org/json/regnum-groups.json
+curl -o _data/remote/regnum.json https://dis.drachenwald.sca.org/data/regnum-officers-box.json
+curl https://dis.drachenwald-sca.org/data/calendar.json |  sed -e ':a' -e ' s/\\n\\n\\n//g; ta' | sed -e ':a' -e ' s/\\n/\<br \/\>/g; ta' > _data/remote/calendar.json 
+curl -o _data/remote/groups.json https://dis.drachenwald.sca.org/data/regnum-groups.json
 curl -L -o _data/remote/hamlets.csv https://docs.google.com/spreadsheets/d/e/2PACX-1vTwqFGCuJn7X5jEFJ9_qhMeGYm6B7QQ0JLipINvNUBHalXkQ_C8RlG9FIQtKlMPHr907dGla_Svpqw4/pub\?gid=0\&single=true\&output=csv
 JEKYLL_ENV=production bundle exec jekyll build
