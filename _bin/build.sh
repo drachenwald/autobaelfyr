d5bdaf8d4eb57d2ec2ce466f1f4a155b6f3ee71c
Anna Wilson <git@annawilson.eu>
2022-10-29 18:49:52 +0100

    use environments

--- _bin/build.sh
+++ _bin/build.sh
@@ -2,4 +2,4 @@ curl -o _data/remote/regnum.json https://dis.drachenwald.sca.org/data/regnum-off
 curl https://dis.drachenwald.sca.org/data/calendar.json |  sed -e ':a' -e ' s/\\n\\n\\n//g; ta' | sed -e ':a' -e ' s/\\n/\<br \/\>/g; ta' > _data/remote/calendar.json 
 curl -o _data/remote/groups.json https://dis.drachenwald.sca.org/data/regnum-groups.json
 curl -L -o _data/remote/hamlets.csv https://docs.google.com/spreadsheets/d/e/2PACX-1vTwqFGCuJn7X5jEFJ9_qhMeGYm6B7QQ0JLipINvNUBHalXkQ_C8RlG9FIQtKlMPHr907dGla_Svpqw4/pub\?gid=0\&single=true\&output=csv
-JEKYLL_ENV=production bundle exec jekyll build
+bundle exec jekyll build
